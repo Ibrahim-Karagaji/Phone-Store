8bf22233ed38fb9eeca9af5c2728b045b5ed5c2d
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-02-22 01:37:49 +0300

    deleted header element (it will be done by better way leter)

--- phone-store/src/App.jsx
+++ phone-store/src/App.jsx
@@ -1,4 +1,11 @@
 import "./App.css";
+import Header from "./Header";
+import Sidebar from "./SideBar";
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="overflow-hidden grid grid-cols-[89%_10%] text-[#eeeeeed4] justify-between !p-[3px]">
+      <Header />
+      <Sidebar />
+    </div>
+  );
 }
