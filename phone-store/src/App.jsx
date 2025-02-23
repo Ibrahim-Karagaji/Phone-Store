c3ea2a8cbe83d7f0051efb302dc110fe9570f643
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-02-23 17:02:13 +0300

    import Header component in app file

--- phone-store/src/App.jsx
+++ phone-store/src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import Header from "./Header";
-import Sidebar from "./SideBar";
 export default function App() {
   return (
-    <div className="overflow-hidden grid grid-cols-[89%_10%] text-[#eeeeeed4] justify-between !p-[3px]">
+    <div className="relative grid grid-cols-[100%] text-[#eeeeeed4] !p-[3px] shadow-[0px_0px_5px_0px_black] rounded-[10px] w-[90%]">
       <Header />
-      <Sidebar />
     </div>
   );
 }
