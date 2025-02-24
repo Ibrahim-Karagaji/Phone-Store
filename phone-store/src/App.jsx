9442cd3fc3ca773d80a1746a7eeac3c7c63f8985
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-02-24 22:03:41 +0300

    deleted app style and the Header component

--- phone-store/src/App.jsx
+++ phone-store/src/App.jsx
@@ -1,9 +1,5 @@
 import "./App.css";
-import Header from "./Header";
+
 export default function App() {
-  return (
-    <div className="relative grid grid-cols-[100%] text-[#eeeeeed4] !p-[3px] shadow-[0px_0px_5px_0px_black] rounded-[10px] w-[90%]">
-      <Header />
-    </div>
-  );
+  return <div></div>;
 }
