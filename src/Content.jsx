ec00ff0dbefbefa35cddda5d89da1ae486393901
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-04 16:59:50 +0300

    create content component to separateSidbar component and phone cards

---
+++ src/Content.jsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+  return (
+    <div className="flex !p-[3px] text-[18px] text-[#eeeeeed1] gap-[20px] min-w-[480px]">
+      <div className="basis-[88%] shadow-md border-[#eeeeeed1] border-[solid] border-[2px] rounded-[5px]"></div>
+    </div>
+  );
+}
