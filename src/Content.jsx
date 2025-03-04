8d255eee77fcebae1ddedae081bd1a297bdfba72
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-04 17:01:20 +0300

    import Sidbar component in conent component

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,8 @@
-export default function SideBar() {
+import SideBar from "./SideBar";
+export default function Content() {
   return (
     <div className="flex !p-[3px] text-[18px] text-[#eeeeeed1] gap-[20px] min-w-[480px]">
+      <SideBar />
       <div className="basis-[88%] shadow-md border-[#eeeeeed1] border-[solid] border-[2px] rounded-[5px]"></div>
     </div>
   );
