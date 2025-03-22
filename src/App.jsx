4f8a79a356b9214c601cf3040fd07daea672d6db
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-23 02:19:18 +0300

    used context hook to share the products with the all component in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
+import useProducts from "./useProducts";
 import { createContext } from "react";
 export const products = createContext();
+
 export default function App() {
   return (
-    <div id="App" className="h-screen">
-      <Header />
-      <Content />
-    </div>
+    <products.Provider value={useProducts()}>
+      <div id="App" className="h-screen">
+        <Header />
+        <Content />
+      </div>
+    </products.Provider>
   );
 }
