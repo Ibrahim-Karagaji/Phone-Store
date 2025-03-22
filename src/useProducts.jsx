8370cbe48d4f9245d6a916c0e925114ccbac4b1d
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-23 02:04:45 +0300

    get the products from the APi

--- src/useProducts.jsx
+++ src/useProducts.jsx
@@ -1,3 +1,19 @@
 import { useEffect, useState } from "react";
 
-export default function useProducts() {}
+export default function useProducts() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch("https://phones-api.deno.dev/phones")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else throw new Error("Network response was not ok");
+      })
+      .then((data) => setProduct(data))
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []);
+  return product;
+}
