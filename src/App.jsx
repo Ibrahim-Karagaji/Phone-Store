488356108f607249222eaff279f887b7b6a56064
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-23 02:11:05 +0300

    create createContext in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
+import { createContext } from "react";
+export const products = createContext();
 export default function App() {
   return (
     <div id="App" className="h-screen">
