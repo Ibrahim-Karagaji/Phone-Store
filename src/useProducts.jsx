f1459a89c24fe2bfaa04606a0fa935131df917e7
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-23 02:05:13 +0300

    get the products from the APi

--- src/useProducts.jsx
+++ src/useProducts.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 
 export default function useProducts() {
-  const [product, setProduct] = useState(null);
-
+  const [product, setProduct] = useState({
+    product: [],
+    isLoading: true,
+    error: false,
+  });
   useEffect(() => {
     fetch("https://phones-api.deno.dev/phones")
       .then((response) => {
         if (response.ok) {
+          setProduct((Prey) => ({ ...Prey, isLoading: false }));
           return response.json();
         } else throw new Error("Network response was not ok");
       })
-      .then((data) => setProduct(data))
+      .then((data) => setProduct((Prey) => ({ ...Prey, product: data })))
       .catch((error) => {
+        setProduct((Prey) => ({ ...Prey, error: true }));
         console.error("There was a problem with the fetch operation:", error);
       });
   }, []);
