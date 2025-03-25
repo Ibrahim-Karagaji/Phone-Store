821ab6716eef7143b4dd706f52bb54ecfefa12ac
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-25 04:28:42 +0300

    finished is loading Phone card component

--- src/IsLoadingPhoneCard.jsx
+++ src/IsLoadingPhoneCard.jsx
@@ -1,3 +1,17 @@
 export default function IsLoadingPhoneCard() {
-  return <div></div>;
+  return (
+    <div className=" rounded-[5px] shadow-[0px_0px_10px_0px_black] grid gap-[3px] w-[fit-content]">
+      <div className="w-[190px] h-[150px] bg-[#031c27]"></div>
+      <div className="grid gap-[5px] !p-[5px]">
+        <h1 className="bg-[#031c27] rounded-[5px] text-[#031c27]">? </h1>
+        <p className="bg-[#031c27] rounded-[5px] text-[#031c27]">?</p>
+      </div>
+      <div className="flex justify-between !p-[5px] text-[#031c27] bg-[#031c27] items-center">
+        <i className="fa-solid heart fa-heart"></i>
+        <button className="flex items-center gap-[8px]">
+          <i className="fa-solid heart fa-heart"></i> <p>Add To Cart</p>
+        </button>
+      </div>
+    </div>
+  );
 }
