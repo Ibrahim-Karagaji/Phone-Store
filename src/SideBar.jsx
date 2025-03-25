e5a07fb394d8d9562ace736e9ccbf5ff28c0e726
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-03-25 05:59:01 +0300

    update SideBar Component (deleted the border and and margin and padding and made the element to the right)

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,36 +1,33 @@
 export default function SideBar() {
   return (
-    <div className="flex !p-[3px] text-[18px] text-[#eeeeeed1] gap-[20px] min-w-[480px]">
-      <div className="min-w-[170px] basis-[12%] shadow-[0px_0px_10px_0px_black] border-[#eeeeeed1 solid] border-[2px] rounded-[5px]">
-        <i className="fa-solid fa-circle-user !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] duration-500 rounded-[3px] !flex w-[fit-content] !ml-[auto] text-[35px]"></i>
-        <div className="content grid gap-[3px] text-[#eeeeeed1] !m-[10px_0px_0px]">
-          <div className="flex justify-center gap-[10px] items-center rounded-[2px] !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-            <p>Best Selaing</p>
-            <i className="fa-solid fa-fire"></i>
-          </div>
-          <div className="flex justify-center gap-[10px] items-center rounded-[2px] !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-            <p>Phone List</p>
-            <i className="fa-solid fa-list"></i>
-          </div>
-          <div className="flex justify-center gap-[10px] items-center rounded-[2px] !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-            <p>Flagship</p>
-            <i className="fa-solid fa-rocket"></i>
-          </div>
-          <div className="flex justify-center gap-[10px] items-center rounded-[2px] !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-            <p>Flagship Killer</p>
-            <i className="fa-solid fa-star"></i>
-          </div>
-          <div className="flex justify-center gap-[10px] items-center rounded-[2px] !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-            <p>Medium</p>
-            <i className="fa-solid fa-star-half"></i>
-          </div>
-          <div className="flex justify-center gap-[10px] items-center rounded-[2px] !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-            <p>Economy</p>
-            <i className="fa-solid fa-lines-leaning"></i>
-          </div>
+    <div className="min-w-[155px] basis-[12%] shadow-[0px_0px_10px_0px_black] rounded-[5px]">
+      <i className="fa-solid fa-circle-user !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] duration-500 rounded-[3px] !flex w-[fit-content] !ml-[auto] text-[35px]"></i>
+      <div className="content grid gap-[3px] text-[#eeeeeed1] !m-[10px_0px_0px]">
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px] !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Best Selaing</p>
+          <i className="fa-solid fa-fire"></i>
+        </div>
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Phone List</p>
+          <i className="fa-solid fa-list"></i>
+        </div>
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Flagship</p>
+          <i className="fa-solid fa-rocket"></i>
+        </div>
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Flagship Killer</p>
+          <i className="fa-solid fa-star"></i>
+        </div>
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Medium</p>
+          <i className="fa-solid fa-star-half"></i>
+        </div>
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Economy</p>
+          <i className="fa-solid fa-lines-leaning"></i>
         </div>
       </div>
-      <div className="basis-[88%] shadow-md border-[#eeeeeed1] border-[solid] border-[2px] rounded-[5px]"></div>
     </div>
   );
 }
