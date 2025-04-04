e5e10e43d4bc773815c764b606398888d7a3fbe9
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-05 02:35:25 +0300

    handled appearing of isLoadingphonecard component , it appears when the api recqust is loading

--- src/PhoneCards.jsx
+++ src/PhoneCards.jsx
@@ -1,11 +1,30 @@
-import DisplayPeoducts from "./DisplayPeoducts"
+import DisplayPeoducts from "./DisplayPeoducts";
+import IsLoadingPhoneCard from "./IsLoadingPhoneCard";
 import { products } from "./App";
 import { useContext } from "react";
 export default function PhoneCards() {
-  return (
-    <div className="flex flex-col gap-5">
-      <DisplayPeoducts />
-      <DisplayPeoducts />
+  const pro = useContext(products);
+  console.log(pro.products)
+  return (pro.isLoading == false ?
+    <div className="grid gap-5">
+
     </div>
+    :
+    pro.error == false ?
+      <div className="grid gap-5">
+        <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
+          <div className="w-[150px] h-[35px] bg-[#031c27] rounded-[5px]"></div>
+          <div className="flex gap-10 ">
+            {[1, 2, 3, 4, 5, 6].map((number, index) => <IsLoadingPhoneCard key={index} />)}
+          </div>
+        </div>
+        <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
+          <div className="w-[150px] h-[35px] bg-[#031c27] rounded-[5px]"></div>
+          <div className="flex gap-10 ">
+            {[1, 2, 3, 4, 5, 6].map((number, index) => <IsLoadingPhoneCard key={index} />)}
+          </div>
+        </div>
+      </div> :
+      console.log("Erorr")
   );
 }
