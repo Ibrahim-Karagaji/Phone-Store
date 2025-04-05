0e39ca7298997a126279b9a08fc1616cbf773ddf
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-05 03:05:34 +0300

    added settings option to the sidbar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,6 +1,6 @@
 export default function SideBar() {
   return (
-    <div className="min-w-[155px] basis-[12%] shadow-[0px_0px_10px_0px_black] rounded-[5px]">
+    <div className="min-w-[155px] basis-[12%] shadow-[0px_0px_5px_0px_black] rounded-[5px]">
       <i className="fa-solid fa-circle-user !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] duration-500 rounded-[3px] !flex w-[fit-content] !ml-[auto] text-[35px]"></i>
       <div className="content grid gap-[3px] text-[#eeeeeed1] !m-[10px_0px_0px]">
         <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px] !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
@@ -26,6 +26,10 @@ export default function SideBar() {
         <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
           <p>Economy</p>
           <i className="fa-solid fa-lines-leaning"></i>
+        </div>
+        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+          <p>Settings</p>
+          <i class="fa-solid fa-gear"></i>
         </div>
       </div>
     </div>
