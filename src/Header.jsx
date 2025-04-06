bc4b659e42ec6491f7e0bf06dfcc5bde12b31ea2
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-07 01:31:00 +0300

    made log in option a link to login component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="flex items-center !mb-[10px] gap-[20px] text-[#eeeeeed1] !p-[3px]">
@@ -12,10 +13,10 @@ export default function Header() {
           className="focus:outline-none w-[93%]"
         />
         <div className="choices flex items-center gap-[10px] !m-[0px_10px]">
-          <a href="https://github.com/Ibrahim-Karagaji"> <i className="fa-solid shopping fa-cart-shopping !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1 relative"></i></a>
+          <i className="fa-solid shopping fa-cart-shopping !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1 relative"></i>
           <i className="fa-solid heart fa-heart !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1 relative"></i>
-          <i className="fa-solid log-in fa-right-to-bracket !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1 relative"></i>
-          <i className="option fa-solid fa-bars left-[0px] absolute opacity-0 !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1"></i>
+          <Link to="/Login"> <i className="fa-solid log-in fa-right-to-bracket !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1 relative"></i>
+            <i className="option fa-solid fa-bars left-[0px] absolute opacity-0 !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[5px] duration-500 hover:-translate-y-1"></i></Link>
 
           <div className="list-option absolute opacity-0 grid bg-[#eeeeeed1] text-[#063447] !p-[2px] text-[2px] top-[-30px] right-[15px] rounded-[5px] duration-500">
             <p className=" !p-[5px] border-b-[2px] duration-500 hover:-translate-y-1">
