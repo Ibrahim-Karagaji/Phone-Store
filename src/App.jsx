c2470d0c4ddeff84d1c99161dfbc10dbd8e41635
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-07 01:08:18 +0300

    put the app as route

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import Content from "./Content";
 import useProducts from "./useProducts";
 import { createContext } from "react";
 import Footer from "./Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export const products = createContext();
 
 export default function App() {
   return (
-    <products.Provider value={useProducts()}>
-      <div id="App" className="h-screen min-w-[640px]">
-        <Header />
-        <Content />
-        <Footer />
-      </div>
-    </products.Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <products.Provider value={useProducts()}>
+            <div id="App" className="h-screen min-w-[640px]">
+              <Header />
+              <Content />
+              <Footer />
+            </div>
+          </products.Provider>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
