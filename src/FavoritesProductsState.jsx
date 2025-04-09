4c8feaddaf5f1c52eed1248cd299ef3c03bd0ebc
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-09 16:51:43 +0300

    create FavoritesProductsState component to be Favorites Provider

---
+++ src/FavoritesProductsState.jsx
@@ -0,0 +1,5 @@
+import { createContext, useState } from "react-router-dom";
+
+export const favoritesProductsContext = createContext();
+
+export default function FavoritesProductsState({ children }) {}
