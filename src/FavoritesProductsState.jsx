590630e00455966331bd58058726713c2a444313
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-11 22:07:03 +0300

    create compoent to becomes a state to the ptoducts that added to the favorites

--- src/FavoritesProductsState.jsx
+++ src/FavoritesProductsState.jsx
@@ -1,5 +1,19 @@
-import { createContext, useState } from "react-router-dom";
+import { createContext, useState } from "react";
 
 export const favoritesProductsContext = createContext();
 
-export default function FavoritesProductsState({ children }) {}
+export default function FavoritesProductsState({ children }) {
+  const [favoritesState, setFavoritesState] = useState([]);
+  function addToFavorites() {}
+  function deleteFromFavorites() {}
+  const state = {
+    favoritesState: favoritesState,
+    addToFavorites,
+    deleteFromFavorites,
+  };
+  return (
+    <favoritesProductsContext.Provider value={state}>
+      {children}
+    </favoritesProductsContext.Provider>
+  );
+}
