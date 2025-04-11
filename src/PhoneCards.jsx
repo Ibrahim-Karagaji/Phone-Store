f58b2eec6febd3d2e3d1269e0db848336650adb0
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-11 23:08:11 +0300

    deleted get random catagory function in phone cards

--- src/PhoneCards.jsx
+++ src/PhoneCards.jsx
@@ -1,26 +1,23 @@
 import DisplayPeoducts from "./DisplayPeoducts";
 import IsLoadingPhoneCard from "./IsLoadingPhoneCard";
-import { products } from "./App";
+import { products } from "./Layout";
 import { useContext } from "react";
+
 export default function PhoneCards() {
   const pro = useContext(products);
 
-  function getRandomBrand() {
-    const brand = ['OnePlus', 'Samsung', 'Apple', 'Honor'];
-
-    const randomBrand = brand[Math.floor(Math.random() * brand.length)];
-
-    return randomBrand;
-  }
-
-  return (pro.isLoading == false ?
+  return pro.isLoading == false ? (
     <div className="grid gap-5">
       <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
         <div className="!p-[5px] bg-[#eeeeeed1] text-[#063447] rounded-[5px] w-[fit-content] font-bold">
           <h1>Best Selaing</h1>
         </div>
         <div className="flex gap-10 ">
-          {DisplayPeoducts(pro.products.filter((p) => p.price <= 900 && p.price >= 400 && p.brand == getRandomBrand()).slice(0, 6))}
+          {DisplayPeoducts(
+            pro.products
+              .filter((p) => p.price <= 900 && p.price >= 400)
+              .slice(0, 6)
+          )}
         </div>
       </div>
       <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
@@ -28,7 +25,9 @@ export default function PhoneCards() {
           <h1>Flagship</h1>
         </div>
         <div className="flex gap-10 ">
-          {DisplayPeoducts(pro.products.filter((p) => p.category == "Flagship" && p.brand == getRandomBrand()).slice(0, 6))}
+          {DisplayPeoducts(
+            pro.products.filter((p) => p.category == "Flagship").slice(0, 6)
+          )}
         </div>
       </div>
       <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
@@ -36,7 +35,11 @@ export default function PhoneCards() {
           <h1>Flagship Killer</h1>
         </div>
         <div className="flex gap-10 ">
-          {DisplayPeoducts(pro.products.filter((p) => p.price <= 900 && p.price >= 500 && p.brand == getRandomBrand()).slice(0, 6))}
+          {DisplayPeoducts(
+            pro.products
+              .filter((p) => p.price <= 900 && p.price >= 500)
+              .slice(0, 6)
+          )}
         </div>
       </div>
       <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
@@ -44,7 +47,9 @@ export default function PhoneCards() {
           <h1>Medium</h1>
         </div>
         <div className="flex gap-10 ">
-          {DisplayPeoducts(pro.products.filter((p) => p.category == "Mid-range").slice(0, 6))}
+          {DisplayPeoducts(
+            pro.products.filter((p) => p.category == "Mid-range").slice(0, 6)
+          )}
         </div>
       </div>
       <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
@@ -52,29 +57,35 @@ export default function PhoneCards() {
           <h1>Economy</h1>
         </div>
         <div className="flex gap-10 ">
-          {DisplayPeoducts(pro.products.filter((p) => p.category == "Budget").slice(0, 6))}
+          {DisplayPeoducts(
+            pro.products.filter((p) => p.category == "Budget").slice(0, 6)
+          )}
         </div>
       </div>
     </div>
-    :
-    pro.error == false ?
-      <div className="grid gap-5">
-        <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
-          <div className="w-[150px] h-[35px] bg-[#031c27] rounded-[5px]"></div>
-          <div className="flex gap-10 ">
-            {[1, 2, 3, 4, 5, 6].map((number, index) => <IsLoadingPhoneCard key={index} />)}
-          </div>
+  ) : pro.error == false ? (
+    <div className="grid gap-5">
+      <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
+        <div className="w-[150px] h-[35px] bg-[#031c27] rounded-[5px]"></div>
+        <div className="flex gap-10 ">
+          {[1, 2, 3, 4, 5, 6].map((number, index) => (
+            <IsLoadingPhoneCard key={index} />
+          ))}
         </div>
-        <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
-          <div className="w-[150px] h-[35px] bg-[#031c27] rounded-[5px]"></div>
-          <div className="flex gap-10 ">
-            {[1, 2, 3, 4, 5, 6].map((number, index) => <IsLoadingPhoneCard key={index} />)}
-          </div>
-        </div>
-      </div> :
-      <div className="grid justify-center gap-10 content-center w-[100%]">
-        <i class="fa-solid fa-face-sad-tear text-[200px]"></i>
-        <h1 className="text-[30px]">404 Not Found</h1>
       </div>
+      <div className="grid gap-5 !p-[10px] shadow-[0px_0px_5px_0px_black] overflow-auto">
+        <div className="w-[150px] h-[35px] bg-[#031c27] rounded-[5px]"></div>
+        <div className="flex gap-10 ">
+          {[1, 2, 3, 4, 5, 6].map((number, index) => (
+            <IsLoadingPhoneCard key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="grid justify-center gap-10 content-center w-[100%]">
+      <i className="fa-solid fa-face-sad-tear text-[200px]"></i>
+      <h1 className="text-[30px]">404 Not Found</h1>
+    </div>
   );
 }
