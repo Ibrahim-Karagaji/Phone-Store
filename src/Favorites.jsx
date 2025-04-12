fc71db12021d8c445bf2e173a830b05390db0cf4
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-13 00:43:56 +0300

    Favorite component is handled and done

--- src/Favorites.jsx
+++ src/Favorites.jsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import DisplayPeoducts from "./DisplayPeoducts";
 import { favoritesProductsContext } from "./FavoritesProductsState";
 import { useContext } from "react";
 
 export default function Favorites() {
-  return (
-    <div className="text-[#eeeeeed1] !p-[3px] min-w-[640px] min-h-[100vh]">
-      <Link to="/">
-        <i className="fa-solid fa-arrow-left text-[25px] hover:text-[#b5b5b5d1]"></i>
-      </Link>
-      <div></div>
+  const favorites = useContext(favoritesProductsContext);
+  return favorites.favoritesState.length == 0 ? (
+    <div
+      className={`shadow-[0px_0px_5px_0px_black] grid gap-3 text-center justify-center rounded-[5px] !m-[auto] w-[fit-content] text-[23px] text-[#eeeeeed1] bg-[#002737]`}
+    >
+      <h1>Favorites is empty</h1>
+      <img src="images-folder\empty.png" className="h-[400px] w-[480px] " />
+    </div>
+  ) : (
+    <div className="grid gap-2 text-[#eeeeeed1] !p-[3px] min-w-[640px] justify-center">
+      <h1 className="text-center text-[23px]">Favorites</h1>
+      <div className="!p-[5px] rounded-[5px]  flex gap-10 overflow-auto w-[fit-content] shadow-[0px_0px_5px_0px_black]">
+        {DisplayPeoducts(favorites.favoritesState)}
+      </div>
     </div>
   );
 }
