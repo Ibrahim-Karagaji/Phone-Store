4d0745906ba8b10f2e698a0ef24ad28f642e28b3
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-12 10:20:33 +0300

    handled handleAddToFavorite

--- src/FavoritesProductsState.jsx
+++ src/FavoritesProductsState.jsx
@@ -4,11 +4,23 @@ export const favoritesProductsContext = createContext();
 
 export default function FavoritesProductsState({ children }) {
   const [favoritesState, setFavoritesState] = useState([]);
-  function addToFavorites() {}
+  function handleAddToFavorites(product) {
+    if (favoritesState.length == 0)
+      setFavoritesState((prev) => {
+        return [...prev, product];
+      });
+    else {
+      const cheek = favoritesState.find((p) => p.name == product.name);
+      if (!cheek)
+        setFavoritesState((prev) => {
+          return [...prev, product];
+        });
+    }
+  }
   function deleteFromFavorites() {}
   const state = {
     favoritesState: favoritesState,
-    addToFavorites,
+    handleAddToFavorites,
     deleteFromFavorites,
   };
   return (
