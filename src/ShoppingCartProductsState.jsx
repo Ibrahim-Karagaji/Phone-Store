9f6c454dddbfa2b44bc4715f02d6ba4d5d35b12c
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-13 23:25:48 +0300

    import useState and createContext hooks created the handlers function but didn't finish yet

--- src/ShoppingCartProductsState.jsx
+++ src/ShoppingCartProductsState.jsx
@@ -1,3 +1,17 @@
+import { useState, createContext } from "react";
+export const shoppingCartProductsContext = createContext();
 export default function ShoppingCartProductsState() {
-  return <div></div>;
+  const [shoppingCartState, setShoppingCartState] = useState([]);
+  function handleAddToShoppingCart() {}
+  function handleDeleteFromShoppingCart() {}
+  const state = {
+    shoppingCartState: shoppingCartState,
+    handleAddToShoppingCart,
+    handleDeleteFromShoppingCart,
+  };
+  return (
+    <shoppingCartProductsContext.Provider
+      value={state}
+    ></shoppingCartProductsContext.Provider>
+  );
 }
