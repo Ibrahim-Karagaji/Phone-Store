ecb8dcc918475dedf92d9822eef1aa7400c95b14
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-15 17:59:04 +0300

    finished ShoppingCartCard component

--- src/LogLn.jsx
+++ src/LogLn.jsx
@@ -3,49 +3,78 @@ import { Link } from "react-router-dom";
 import { useRef } from "react";
 
 export default function LogIn() {
-    const emailInput = useRef(null)
-    const passwordInput = useRef(null)
-    const [userInfo, setUserInfo] = useState({ email: "", password: "" });
-    const exactEmailRegex = /\w+@gmail.com/;
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const exactEmailRegex = /\w+@gmail.com/;
 
-    function handleEmailInout(value) {
-        if (!(exactEmailRegex.test(userInfo.email) == true)) {
-            emailInput.current.style.border = "red 2px solid";
-        }
-        else {
-            emailInput.current.style.border = "green 2px solid";
-        }
-        setUserInfo((prev) => { return { ...prev, email: value } });
+  function handleEmailInout(value) {
+    if (!(exactEmailRegex.test(userInfo.email) == true)) {
+      emailInput.current.style.border = "red 2px solid";
+    } else {
+      emailInput.current.style.border = "green 2px solid";
     }
+    setUserInfo((prev) => {
+      return { ...prev, email: value };
+    });
+  }
 
-    function handlePasswordInout(value) {
-        if (userInfo.password.length <= 8) {
-            passwordInput.current.style.border = "red 2px solid";
-        }
-        else {
-            passwordInput.current.style.border = "green 2px solid";
-        }
-        setUserInfo((prev) => { return { ...prev, password: value } });
+  function handlePasswordInout(value) {
+    if (userInfo.password.length <= 8) {
+      passwordInput.current.style.border = "red 2px solid";
+    } else {
+      passwordInput.current.style.border = "green 2px solid";
     }
+    setUserInfo((prev) => {
+      return { ...prev, password: value };
+    });
+  }
 
-    return (
-        <div className="text-[#eeeeeed1] !p-[3px] min-w-[640px] min-h-[100vh]">
-            <Link to="/">
-                <i className="fa-solid fa-arrow-left text-[25px] !m-[30px] hover:text-[#b5b5b5d1]"></i>
-            </Link>
-            <div className="flex justify-center !m-[100px] text-[#063447]">
-                <div className="!p-[10px] bg-[#eeeeeed1] rounded-[5px] shadow w-[fit-content]">
-                    <div className="flex justify-center gap-2 text-[25px]">
-                        <h1 className="font-bold">Log in</h1>
-                        <i className="fa-solid fa-arrow-right-to-bracket !mt-[10px]"></i>
-                    </div>
-                    <form className="grid gap-4 !mt-[15px]">
-                        <input ref={emailInput} onChange={(event) => { handleEmailInout(event.target.value) }} className="rounded-[5px] bg-[#063447] text-[#eeeeeed1] !p-[8px] w-[350px]" placeholder="example@gmail.com" />
-                        <input ref={passwordInput} onChange={(event) => { handlePasswordInout(event.target.value) }} className="rounded-[5px] bg-[#063447] text-[#eeeeeed1] !p-[8px] w-[350px]" placeholder="Password" />
-                        <button disabled={!(exactEmailRegex.test(userInfo.email) == true && userInfo.password.length >= 8)} className="!p-[5px] bg-[#063447] rounded-[5px] text-[#eeeeeed1] text-[20px] hover:bg-[#042431] duration-500">Log in</button>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div className="text-[#eeeeeed1] !p-[3px] min-w-[640px] min-h-[100vh]">
+      <Link to="/">
+        <i className="fa-solid fa-arrow-left text-[25px] !m-[30px] hover:text-[#b5b5b5d1]"></i>
+      </Link>
+      <div className="flex justify-center !m-[100px] text-[#063447]">
+        <div className="!p-[10px] bg-[#eeeeeed1] rounded-[5px] shadow w-[fit-content]">
+          <div className="flex justify-center gap-2 text-[25px]">
+            <h1 className="font-bold">Log in</h1>
+            <i className="fa-solid fa-arrow-right-to-bracket !mt-[10px]"></i>
+          </div>
+          <form className="grid gap-4 !mt-[15px]">
+            <input
+              type="Email"
+              required
+              ref={emailInput}
+              onChange={(event) => {
+                handleEmailInout(event.target.value);
+              }}
+              className="rounded-[5px] bg-[#063447] text-[#eeeeeed1] !p-[8px] w-[350px]"
+              placeholder="example@gmail.com"
+            />
+            <input
+              required
+              ref={passwordInput}
+              onChange={(event) => {
+                handlePasswordInout(event.target.value);
+              }}
+              className="rounded-[5px] bg-[#063447] text-[#eeeeeed1] !p-[8px] w-[350px]"
+              placeholder="Password"
+            />
+            <button
+              disabled={
+                !(
+                  exactEmailRegex.test(userInfo.email) == true &&
+                  userInfo.password.length >= 8
+                )
+              }
+              className="!p-[5px] bg-[#063447] rounded-[5px] text-[#eeeeeed1] text-[20px] hover:bg-[#042431] duration-500"
+            >
+              Log in
+            </button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/ShoppingCartCard.jsx
+++ src/ShoppingCartCard.jsx
@@ -1,3 +1,68 @@
-export default function ShoppingCartCard() {
-  return <div></div>;
+import { favoritesProductsContext } from "./FavoritesProductsState";
+import { shoppingCartProductsContext } from "./ShoppingCartProductsState";
+import { useContext } from "react";
+export default function ShoppingCartCard({ product, index }) {
+  const favorites = useContext(favoritesProductsContext);
+  const shoppingCart = useContext(shoppingCartProductsContext);
+  return (
+    <div
+      key={index}
+      className="flex gap-3 shadow-[0px_0px_2px_0px_black] !p-[5px] "
+    >
+      <img
+        className="w-[220px] h-[230px]"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiUHB559_lpuhRggsf2SfA6nuCvsM_7zj8lg&s"
+      />
+      <div className="grid gap-1 !p-[3px] ">
+        <p className="text-[19px]">{product.name}</p>
+        <p className="!p-[5px] w-[350px] h-[fit-content] text-[#063447] bg-[#eeeeeed1] leading-[1.8] rounded-[3px]">
+          {product.description}
+        </p>
+        <div className="flex items-center justify-between border-b-[1px] border-b-[#eeeeee40]">
+          <p>${product.price}</p>
+          <img
+            className={`w-[30px] rounded-[3px] !p-[3px] rounded-[5px] ${
+              favorites.favoritesState.some(
+                (fav) => fav.name == product.name
+              ) == true
+                ? `bg-[red]`
+                : `bg-[transparent]`
+            }`}
+            onClick={() => {
+              if (
+                favorites.favoritesState.some(
+                  (fav) => fav.name == product.name
+                ) == true
+              )
+                favorites.deleteFromFavorites(product);
+              else favorites.handleAddToFavorites(product);
+            }}
+            src="images-folder\wishlist-dark.svg"
+          />
+        </div>
+        <div className="flex items-center justify-between !mt-[15px]">
+          <label for="count" className="relative countOption">
+            <select
+              id="count"
+              name="count"
+              className="!pl-[13px] !pr-[20px] !pb-[8px] text-[#eeeeeed1] bg-[#063447] border-b-[#00b2ff] border-b-[1px] "
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </label>
+          <div
+            onClick={() => {
+              shoppingCart.handleDeleteFromShoppingCart(product);
+            }}
+            className="!p-[4px] flex items-center gap-2 text-[red] hover:bg-[#ff000026] duration-300 "
+          >
+            <span>DELETE</span>
+            <i className="fa-solid fa-trash"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
