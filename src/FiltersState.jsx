0eb919d5cf0d91ff39c130882ef542e44138d926
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-20 20:00:07 +0300

    created state of the filltres

--- src/FiltersState.jsx
+++ src/FiltersState.jsx
@@ -1,1 +1,14 @@
-export default function FiltersState() {}
+import { useState, createContext } from "react";
+
+export const filtersStateContext = createContext();
+export default function FiltersState({ children }) {
+  const [filtersState, setFiltersState] = useState([]);
+
+  const state = { filtersState: filtersState };
+
+  return (
+    <filtersStateContext.Provider value={state}>
+      {children}
+    </filtersStateContext.Provider>
+  );
+}
