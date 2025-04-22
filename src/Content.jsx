bda736721dbb7d0fb5591eea2158959c29104d3e
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-22 06:02:59 +0300

    deleted the min width proprety in Content component

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,7 +2,7 @@ import SideBar from "./SideBar";
 import PhoneCards from "./PhoneCards";
 export default function Content() {
   return (
-    <div className="flex !p-[3px] text-[18px] text-[#eeeeeed1] gap-[20px] min-w-[480px]">
+    <div className="flex !p-[3px] text-[18px] text-[#eeeeeed1] gap-[20px]">
       <SideBar />
       <PhoneCards />
     </div>
