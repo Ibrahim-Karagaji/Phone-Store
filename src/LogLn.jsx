feb68e59198963b4a5f4f9aad2051afe47802d40
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-22 06:08:06 +0300

    deleted the min width proprety in login component

--- src/LogLn.jsx
+++ src/LogLn.jsx
@@ -16,7 +16,7 @@ export default function LogIn() {
   }
 
   return (
-    <div className="text-[#eeeeeed1] !p-[3px] min-w-[640px] min-h-[100vh]">
+    <div className="text-[#eeeeeed1] !p-[3px] min-h-[100vh]">
       <Link to="/">
         <i className="fa-solid fa-arrow-left text-[25px] !m-[30px] hover:text-[#b5b5b5d1]"></i>
       </Link>
