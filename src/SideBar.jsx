1b22ba62870d7e20146b2977b862c6d05bc5c0dc
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-23 05:51:04 +0300

    put link to the all options

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,7 +1,8 @@
-import FiltersState from "./FiltersState";
+import { filtersStateContext } from "./FiltersState";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 export default function SideBar() {
+  const filters = useContext(filtersStateContext);
   return (
     <div className="SideBar min-w-[155px] basis-[12%] shadow-[0px_0px_5px_0px_black] rounded-[5px] duration-300 ">
       <i className="fa-solid fa-circle-user !p-[5px] hover:text-[#063447] hover:bg-[#eeeeeed1] duration-500 rounded-[3px] !flex w-[fit-content] !ml-[auto] text-[25px]"></i>
@@ -16,18 +17,33 @@ export default function SideBar() {
             <i className="fa-solid fa-list"></i>
           </div>
         </Link>
-        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-          <p>Flagship</p>
-          <i className="fa-solid fa-rocket"></i>
-        </div>
-        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-          <p>Mid-range</p>
-          <i className="fa-solid fa-star"></i>
-        </div>
-        <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
-          <p>Budget</p>
-          <i className="fa-solid fa-lines-leaning"></i>
-        </div>
+        <Link
+          onClick={() => filters.handleAddCategoryState("Flagship")}
+          to="/List-Phones"
+        >
+          <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
+            <p>Flagship</p>
+            <i className="fa-solid fa-rocket"></i>
+          </div>
+        </Link>
+        <Link to="/List-Phones">
+          <div
+            onClick={() => filters.handleAddCategoryState("Mid-range")}
+            className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500"
+          >
+            <p>Mid-range</p>
+            <i className="fa-solid fa-star"></i>
+          </div>
+        </Link>
+        <Link to="/List-Phones">
+          <div
+            onClick={() => filters.handleAddCategoryState("Budget")}
+            className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500"
+          >
+            <p>Budget</p>
+            <i className="fa-solid fa-lines-leaning"></i>
+          </div>
+        </Link>
         <div className="flex justify-end gap-[10px] items-center rounded-[2px] !pr-[10px]  !mb-2 !p-[3px] bg-[#063447] hover:bg-[#eeeeeed1] hover:text-[#063447] duration-500">
           <p>Settings</p>
           <i className="fa-solid fa-gear"></i>
