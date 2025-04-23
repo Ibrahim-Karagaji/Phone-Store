0e1896f1dc54bac519ede1701fd4e7c7a15e8c63
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-04-23 19:56:17 +0300

    import ProductDetailsState.jsx component in Layout file

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,21 +6,27 @@ import { Outlet } from "react-router-dom";
 import FavoritesProductsState from "./FavoritesProductsState";
 import { createContext } from "react";
 import ShoppingCartProductsState from "./ShoppingCartProductsState";
+import FiltersState from "./FiltersState";
+import ProductDetailsState from "./ProductDetailsState";
 
 export const products = createContext();
 
 export default function Layout() {
   return (
     <products.Provider value={useProducts()}>
-      <ShoppingCartProductsState>
-        <FavoritesProductsState>
-          <div id="App" className="h-screen min-w-[600px]">
-            <Header />
-            <Outlet />
-            <Footer />
-          </div>
-        </FavoritesProductsState>
-      </ShoppingCartProductsState>
+      <ProductDetailsState>
+        <FiltersState>
+          <ShoppingCartProductsState>
+            <FavoritesProductsState>
+              <div id="App" className="h-screen">
+                <Header />
+                <Outlet />
+                <Footer />
+              </div>
+            </FavoritesProductsState>
+          </ShoppingCartProductsState>
+        </FiltersState>
+      </ProductDetailsState>
     </products.Provider>
   );
 }
