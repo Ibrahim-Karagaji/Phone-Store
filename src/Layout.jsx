09a66cf577128fc512984a1a2cf324f2fac4fa3d
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-05-05 02:55:20 +0300

    import searchState component in layout component to make it a provider to the all component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import "./App.css";
 import Header from "./Header";
 import useProducts from "./useProducts";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import FavoritesProductsState from "./FavoritesProductsState";
-import { createContext } from "react";
 import ShoppingCartProductsState from "./ShoppingCartProductsState";
 import FiltersState from "./FiltersState";
 import ProductDetailsState from "./ProductDetailsState";
 export const products = createContext();
 import WelcomeMassage from "./WelcomeMassage";
+import SearchState from "./SearchState";
 
 export default function Layout() {
   const [waiting, setWaiting] = useState(false);
@@ -21,25 +21,27 @@ export default function Layout() {
 
   return (
     <products.Provider value={useProducts()}>
-      <ProductDetailsState>
-        <FiltersState>
-          <ShoppingCartProductsState>
-            <FavoritesProductsState>
-              <div id="App" className="h-screen">
-                {waiting == false ? (
-                  <WelcomeMassage />
-                ) : (
-                  <>
-                    <Header />
-                    <Outlet />
-                    <Footer />
-                  </>
-                )}
-              </div>
-            </FavoritesProductsState>
-          </ShoppingCartProductsState>
-        </FiltersState>
-      </ProductDetailsState>
+      <SearchState>
+        <ProductDetailsState>
+          <FiltersState>
+            <ShoppingCartProductsState>
+              <FavoritesProductsState>
+                <div id="App" className="h-screen">
+                  {waiting == false ? (
+                    <WelcomeMassage />
+                  ) : (
+                    <>
+                      <Header />
+                      <Outlet />
+                      <Footer />
+                    </>
+                  )}
+                </div>
+              </FavoritesProductsState>
+            </ShoppingCartProductsState>
+          </FiltersState>
+        </ProductDetailsState>
+      </SearchState>
     </products.Provider>
   );
 }
