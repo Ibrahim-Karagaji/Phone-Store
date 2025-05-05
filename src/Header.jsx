21e7875189ab9f3eccf184f4f599a1b961627631
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-05-05 03:42:47 +0300

    import SearchStateContext state in header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,24 @@
+import { SearchStateContext } from "./SearchState";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 export default function Header() {
+  const search = useContext(SearchStateContext);
   return (
     <div className="flex items-center gap-[10px] text-[#eeeeeed1] !p-[3px]">
       <img
         src="images-folder\site-Logo.png"
         className="shadow-[0px_0px_2px_0px_black] basis-[5%] w-[100px] h-[65px] rounded-[5px]"
       />
-      <div className="shadow-[0px_0px_2px_0px_black] text-[18px] h-[100%] basis-[95%] gap-[10px] flex items-center rounded-[5px]">
+      <div className="relative shadow-[0px_0px_2px_0px_black] text-[18px] h-[100%] basis-[95%] gap-[10px] flex items-center rounded-[5px]">
         <i className="fa-solid fa-magnifying-glass text-[#063447] bg-[#eeeeeed1] !p-[20px] rounded-[3px]"></i>
         <input
+          onInput={(e) => {
+            search.handleSetSearch(e.target.value);
+          }}
           placeholder="Search for a Phone"
           className="focus:outline-none w-[93%]"
         />
+        SearchStateContext{" "}
         <div className="choices flex items-center gap-[5px] !m-[0px_10px]">
           <Link to="/">
             <i className="fa-solid fa-house home !p-[3px] text-[16px] hover:text-[#063447] hover:bg-[#eeeeeed1] rounded-[4px] duration-300 hover:-translate-y-1 relative"></i>
