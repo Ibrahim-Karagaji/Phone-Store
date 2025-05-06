d9c18c26a154f2506cca4ada874beced2aea84d5
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-05-07 01:36:33 +0300

    AddToCartElement is done

--- src/AddToCartElement.jsx
+++ src/AddToCartElement.jsx
@@ -1,5 +1,47 @@
+import { useContext } from "react";
+import { shoppingCartProductsContext } from "./ShoppingCartProductsState";
 import React from "react";
+export default function AddToCartElement({ product }) {
+  const shoppingCart = useContext(shoppingCartProductsContext);
 
-export default function AddToCartElement() {
-  return <div>AddToCartElement</div>;
+  return (
+    <>
+      <button
+        onClick={() => {
+          if (
+            !shoppingCart.shoppingCartState.some(
+              (shop) => shop.name != product.name
+            )
+          );
+          shoppingCart.handleAddToShoppingCart(p);
+        }}
+        className={`flex items-center gap-[8px] rounded-[3px] !pl-[8px] !pr-[5px] duration-500 ${
+          shoppingCart.shoppingCartState.some(
+            (shop) => shop.name == product.name
+          ) == true
+            ? `!bg-[#002737] text-[#00b2ff] hover:bg-[#194d63]`
+            : `bg-[#eeeeeed4] hover:bg-[#eeeeee96] text-[#063447]`
+        }`}
+      >
+        <i
+          className={
+            shoppingCart.shoppingCartState.some(
+              (shop) => shop.name == product.name
+            ) == true
+              ? `fa-solid fa-cart-plus`
+              : `fa-solid fa-cart-shopping`
+          }
+        ></i>
+        {shoppingCart.shoppingCartState.some(
+          (shop) => shop.name == product.name
+        ) == true ? (
+          <Link to="/shopping-cart">
+            <p className="font-semibold">Go To Cart</p>
+          </Link>
+        ) : (
+          <p className="font-semibold">Add To Cart</p>
+        )}
+      </button>
+    </>
+  );
 }
