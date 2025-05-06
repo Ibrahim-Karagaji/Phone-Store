311351f6198204debc040ebe493c7dd928b79c22
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-05-07 00:13:41 +0300

    move handleAddToFavorites function to FavoritesProductsState component

--- src/FavoritesProductsState.jsx
+++ src/FavoritesProductsState.jsx
@@ -4,7 +4,22 @@ export const favoritesProductsContext = createContext();
 
 export default function FavoritesProductsState({ children }) {
   const [favoritesState, setFavoritesState] = useState([]);
+  let fav = window.localStorage.getItem("favorites");
+
   function handleAddToFavorites(product) {
+    if (window.localStorage.getItem("favorites") == null) {
+      let favorites = [];
+      favorites.push(product);
+      window.localStorage.setItem("favorites", JSON.stringify(favorites));
+    } else {
+      let convert = JSON.parse(fav);
+      const check = convert.some((p) => p.name == product.name);
+      if (!check) {
+        convert.push(product);
+        window.localStorage.setItem("favorites", JSON.stringify(convert));
+      }
+    }
+
     if (favoritesState.length == 0)
       setFavoritesState((prev) => {
         return [...prev, product];
@@ -17,10 +32,12 @@ export default function FavoritesProductsState({ children }) {
         });
     }
   }
+
   function deleteFromFavorites(product) {
     const fav = favoritesState.filter((p) => p.name != product.name);
     setFavoritesState(fav);
   }
+
   const state = {
     favoritesState: favoritesState,
     handleAddToFavorites,
