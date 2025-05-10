9f07cddf24f30ea343e8aabb99532d2d82066a10
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-05-10 20:41:01 +0300

    get the ptoducts that have added to the shopping cart from the localStorge

--- src/Chekout.jsx
+++ src/Chekout.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import Footer from "./Footer";
 
 export default function Chekout() {
+  const getShoppingcartProducts = window.localStorage.getItem("shoppingCart");
+  const shoppingCart = JSON.parse(getShoppingcartProducts);
+
   return (
     <div className=" text-[#eeeeeed1] !p-[4px]">
       <div className="flex justify-between items-center shadow bg-[#eeeeeed1] text-[#063447] rounded-[5px] !p-[3px]">
