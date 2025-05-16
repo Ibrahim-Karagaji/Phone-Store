d0f912afb554beae4966364332af86017548c32b
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-05-16 11:54:18 +0300

    shrunk size of the texts in the images when the screen get smaller

--- src/DisplayPeoducts.jsx
+++ src/DisplayPeoducts.jsx
@@ -9,7 +9,7 @@ export default function DisplayPeoducts(products) {
     return (
       <div
         key={index}
-        className="DisplayPeoducts text-[15px] text-[#eeeeeed4] rounded-[5px] shadow-[0px_0px_2px_0px_black] grid  w-[fit-content] duration-300 hover:-translate-y-1"
+        className="displayPeoducts text-[15px] text-[#eeeeeed4] rounded-[5px] shadow-[0px_0px_2px_0px_black] grid  w-[fit-content] duration-300 hover:-translate-y-1"
       >
         <Link
           onClick={() => {
@@ -19,19 +19,19 @@ export default function DisplayPeoducts(products) {
         >
           <div className="more-details w-[auto] h-[auto] ">
             <img
-              className="h-full rounded-[5px] max-w-[230px] max-h-[350px]"
+              className="homePageProductsImg h-full rounded-[5px] max-w-[230px] max-h-[350px] !mr-[auto] !ml-[auto]"
               src={p.images[0]}
             />
           </div>
         </Link>
-        <div className="grid gap-[5px] !p-[5px]">
+        <div className="grid gap-[3px] !p-[5px]">
           <h1 className="rounded-[5px] font-bold">{p.name}</h1>
-          <p className="text-[#063447] bg-[#eeeeeed4] w-[220px] !p-[3px] rounded-[5px] font-semibold text-[15px]">
+          <p className="text-[#063447] bg-[#eeeeeed4] max-w-[220px] !p-[3px] rounded-[5px] font-semibold displayPeoductsDescription">
             {p.description}
           </p>
-          <p className="rounded-[5px] font-bold">Price: {p.price + "$"}</p>
+          <p className="rounded-[5px] font-bold">Price: {p.price}$</p>
         </div>
-        <div className="flex justify-between !p-[5px] items-center">
+        <div className="flex justify-between items-center">
           <AddToFavoriteElement product={p} />
           <AddToCartElement product={p} />
         </div>
